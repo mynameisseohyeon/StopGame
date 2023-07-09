b874a0277e4859118dbb4c6f938575322654720a
seohyeon <uimi3606@gmail.com>
2023-07-09 16:00:09 +0900

    FIX 사운드 추가

--- App.jsx
+++ App.jsx
@@ -5,6 +5,15 @@ import MainPage from './screens/MainPage';
 import CharacterSelectPage from './screens/CharacterSelectPage';
 import PlayPage from './screens/PlayPage';
 
+import SoundPlayer from 'react-native-sound-player';
+
+try {
+  SoundPlayer.playSoundFile('mainbgm', 'mp3');
+  SoundPlayer.setNumberOfLoops(-1); //오디오 반복 재생
+} catch (e) {
+  console.log('cannot play the sound file', e);
+}
+
 function App() {
   return (
     <View>
--- assets/BGM.js
+++
@@ -1,7 +0,0 @@
-// import Sound from 'react-native-sound';
-
-// const BGM = {
-//   MainBGM: require('../android/app/src/main/res/raw/MainBGM.mp3'),
-// };
-
-// export default BGM;
